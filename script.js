dbcf54f0306753ac54ea810611834794dc2f3b10
kairatdev <153768194+kairatdev@users.noreply.github.com>
2024-01-18 23:34:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// let body = document.body
+
+// let link = document.createElement('h1')
+// link.innerHTML = 'salam'
+// body.prepend(link)
+
+// let p = document.createElement('p')
+// p.innerHTML='sd'
+// body.prepend(link)
+// body.prepend(p)
+// setTimeout(()=>{
+//     link.remove()
+// },4000);
+
+const createtodo = ()=>{
+let inp = document.getElementById('todotext').value
+let todo = document.createElement('li')
+todo.append(inp)
+text.append(todo)
+
+}
+
+
+
